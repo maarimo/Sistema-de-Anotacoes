e28476a905510ad604afbcb9df6914038cd60913
Matheus Amorim <abmm1234@hotmail.com>
2025-10-21 19:20:09 -0300

    created AnotacaoService

--- src/main/java/com/learning/sistemaAnotacoes/repositories/AnotacaoRepository.java
+++ src/main/java/com/learning/sistemaAnotacoes/repositories/AnotacaoRepository.java
@@ -3,7 +3,8 @@ package com.learning.sistemaAnotacoes.repositories;
 import com.learning.sistemaAnotacoes.entities.Anotacao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
+import java.util.List;
 
 public interface AnotacaoRepository extends JpaRepository<Anotacao, Long> {
+    List<Anotacao> findByFavoritaTrue();
 }
--- src/main/java/com/learning/sistemaAnotacoes/services/AnotacaoService.java
+++ src/main/java/com/learning/sistemaAnotacoes/services/AnotacaoService.java
@@ -1,4 +1,48 @@
 package com.learning.sistemaAnotacoes.services;
 
+import com.learning.sistemaAnotacoes.entities.Anotacao;
+import com.learning.sistemaAnotacoes.exceptions.ResourceNotFoundException;
+import com.learning.sistemaAnotacoes.repositories.AnotacaoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class AnotacaoService {
+    private final AnotacaoRepository repo;
+
+    public AnotacaoService(AnotacaoRepository repo) {this.repo = repo;}
+
+    public List<Anotacao> listar(){
+        return repo.findAll();
+    }
+
+    public Anotacao buscarPorId(Long id){
+        return repo.findById(id).orElseThrow(() -> new ResourceNotFoundException("Anotação não encontrada: " + id));
+    }
+
+    public Anotacao criar(Anotacao anotacao){
+        return repo.save(anotacao);
+    }
+
+    public Anotacao atualizar(Long id, Anotacao anotacao){
+        Anotacao existente = buscarPorId(id);
+
+        existente.setTitulo(anotacao.getTitulo());
+        existente.setConteudo(anotacao.getConteudo());
+        existente.setFavorita(anotacao.getFavorita());
+
+        return repo.save(existente);
+    }
+
+    public void deletar(Long id){
+        if(!repo.existsById(id)){
+            throw new ResourceNotFoundException("Anotação não encontrada: " + id);
+        }
+        repo.deleteById(id);
+    }
+
+    public List<Anotacao> listarFavoritas(){
+        return repo.findByFavoritaTrue();
+    }
 }
